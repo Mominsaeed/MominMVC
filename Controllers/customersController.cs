2be50b41d025c3b2ed0fe6890f3391b698237697
muqeet <muqeetabdul724@gmail.com>
2020-09-25 18:15:14 +0500

    momin

---
+++ Controllers/customersController.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+//using customer.Models;
+using momin.Models;
+
+namespace momin.Controllers
+{
+    public class customersController : Controller
+    {
+        private CustomerDbContext db = new CustomerDbContext();
+
+        // GET: customers
+        public ActionResult Index(string searchString)
+        {
+            var Customers = from c in db.customers
+                         select c;
+
+            if (!String.IsNullOrEmpty(searchString))
+            {
+                Customers = Customers.Where(s => s.name.Contains(searchString));
+            }
+
+            return View(Customers);
+           // return View(db.customers.ToList());
+        }
+
+        // GET: customers/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            customer customer = db.customers.Find(id);
+            if (customer == null)
+            {
+                return HttpNotFound();
+            }
+            return View(customer);
+        }
+
+        // GET: customers/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: customers/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "ID,name,CustomerTYpe")] customer customer)
+        {
+            if (ModelState.IsValid)
+            {
+                db.customers.Add(customer);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(customer);
+        }
+
+        // GET: customers/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            customer customer = db.customers.Find(id);
+            if (customer == null)
+            {
+                return HttpNotFound();
+            }
+            return View(customer);
+        }
+
+        // POST: customers/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "ID,name,CustomerTYpe")] customer customer)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(customer).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(customer);
+        }
+
+        // GET: customers/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            customer customer = db.customers.Find(id);
+            if (customer == null)
+            {
+                return HttpNotFound();
+            }
+            return View(customer);
+        }
+
+        // POST: customers/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            customer customer = db.customers.Find(id);
+            db.customers.Remove(customer);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ Models/customer.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Data.Entity;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace momin.Models
+{
+    public class customer
+    {
+        public int ID { get; set; }
+        public string name { get; set; }
+        public string CustomerTYpe { get; set; }
+
+    }
+    
+    public class CustomerDbContext : DbContext
+    {
+        public DbSet<customer> customers { get; set; }
+      //  public CustomerDbContext db = new CustomerDbContext();
+    }
+ 
+}
